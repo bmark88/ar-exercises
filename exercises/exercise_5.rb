8f37818bd74a2906448bf47dcc24c7a40c6b61ba
bmark88 <bradley.mark@hotmail.com>
2020-04-27 21:43:26 +0000

    Find sum, avg of store revenues. Also find count of store making over 1M/yr

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+total_revenue = Store.sum(:annual_revenue)
+average_revenue = Store.average(:annual_revenue)
+high_revenue_stores = Store.where("annual_revenue > 1000000").count
+
+puts total_revenue
+puts average_revenue
+puts high_revenue_stores
