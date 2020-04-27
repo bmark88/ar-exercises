45fa8fc61bbd4fde5156951d27d9f65c21c27543
bmark88 <bradley.mark@hotmail.com>
2020-04-27 22:03:48 +0000

    Add relation between employees and stores tables, and add employees to store 1 and 2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+Store.has_many :employees
+Employee.belongs_to :store
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jane", last_name: "Smith", hourly_rate: 70)
+@store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 80)
+@store2.employees.create(first_name: "NotJane", last_name: "NotSmith", hourly_rate: 7)
+@store2.employees.create(first_name: "NotJohn", last_name: "NotDoe", hourly_rate: 8)
+
+puts @store1.employees
+puts @store2.employees
