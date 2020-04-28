53fe05e0328dfaf8eecfc0740f7eec33811219c5
bmark88 <bradley.mark@hotmail.com>
2020-04-28 03:59:36 +0000

    Add employee data to store id 3 to allow for error to be raised

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -8,6 +8,14 @@ puts "----------"
 # Your code goes here ...
 
 @store3 = Store.find_by(id: 3)
+
+# Adding code in #'s below to add employees before destroy so error can be raised - otherwise does not work since employees do not exist yet.
+########################
+Store.has_many :employees
+Employee.belongs_to :store
+@store3.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 80)
+########################
+
 @store3.destroy
 
 num_stores = Store.count
