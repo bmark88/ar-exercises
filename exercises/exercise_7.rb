10b51347a2e64dda7d3b499ab9d8288e063f85e1
bmark88 <bradley.mark@hotmail.com>
2020-04-28 01:10:52 +0000

    Add validation and error messages

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,15 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+#This should be a valid db creation and return true since it belongs to a store, has a first and last name, and an hourly_rate between 40 and 200.
+puts @store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 80).valid?
+
+puts Store.create(name: "MyNewStore", annual_revenue: "0", mens_apparel: true, womens_apparel: true).errors.full_messages
+
+puts "~~~~~~~~~~~~~~~~~~~"
+puts "Name your new store!"
+new_store = gets.chomp
+store99 = Store.create(name: new_store)
+
+puts store99.errors.full_messages
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,9 @@
 class Store < ActiveRecord::Base
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}
+  validate :contains_apparel
 
+  def contains_apparel
+    errors.add(:new_stores, "must carry mens or womens apparel") if !mens_apparel && !womens_apparel
+  end
 end
